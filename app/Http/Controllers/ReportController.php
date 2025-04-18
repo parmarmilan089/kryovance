d283ac11f4d5c3c5cc18750167e55fca41500a25
parmarmilan089 <parmarmilan089@gmail.com>
2025-04-18 18:32:26 +0530

    adding

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -15,7 +15,7 @@ class ReportController extends Controller
     //
     public function index() {
         $data = [];
-        $data['title'] = 'Report List';
+        $data['title'] = 'Report Listt';
         $data['menu_active_tab'] = 'report-listt';
 
         return view('admin.report.list')->with($data);
