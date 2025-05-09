689ae0859ef1b568962a6c2ffccb9c42b0f735ae
Milan Parmar <parmarmilan089@gmail.com>
2025-05-09 12:43:07 +0530

    adding new changes

--- resources/views/user/layout/header.blade.php
+++ resources/views/user/layout/header.blade.php
@@ -306,7 +306,6 @@
                                    <div class="search_body">
                                         <div class="container-fluid prl_90">
                                             <form action="{{route('products')}}" method="get">
-<<<<<<< HEAD
 
                                                 <div class="form_item mb-0">
 
@@ -316,17 +315,6 @@
 
                                                 </div>
 
-=======
-                    
-                                                <div class="form_item mb-0">
-                    
-                                                    <input type="search" name="search" placeholder="Type here..."  value="{{ $search ?? '' }}">
-                    
-                                                    <button type="submit"><i class="fal fa-search"></i></button>
-                    
-                                                </div>
-                    
->>>>>>> 7433a7b (adding files)
                                             </form>
                                         </div>
                                     </div>
